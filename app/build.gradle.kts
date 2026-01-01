622c4bee743246d055ba029c6c905ed10042a4c9
Bradley Okore <catersuniversity@yahoo.com>
2026-01-01 14:44:47 +0300

    trying to implement the maps then next is having the dashboard coming a a hamburger menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0") // Updated to a more recent version
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
